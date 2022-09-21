638d5524e7e16fce6588fd1531a8f292b287f9ca
rashmi-4799 <107112111+rashmi-4799@users.noreply.github.com>
2022-09-21 20:10:54 +0530

    Create menuAPI.js

---
+++ menuAPI.js
@@ -0,0 +1,92 @@
+const menuAPI=
+[
+  {
+     id:1,
+     name:"Maggi",
+     image:"image/maggi.jpg",
+     category:"Breakfast",
+     price:"40 Rs.",
+     description:"No matter where you go, no matter how remote a village, no matter what region of India, you will find Maggi 2-Minute Noodles."
+  },
+  {
+    id:2,
+    name:"Noodle",
+    image:"image/noodle.jpg",
+    category:"Breakfast",
+    price:"40 Rs.",
+    description:"Peace will come to the world when the people have enough noodles to eat. · Noodles are not only amusing but delicious."
+  },
+  {
+    id:3,
+    name:"Burger",
+    image:"image/burger.jpg",
+    category:"Fast-food",
+    price:"35 Rs.",
+    description:" Life is too short to eat veg burgers. · When in doubt, take a bite of burger. · Burgers are the best stress buster."
+  },
+  {
+    id:4,
+    name:"Momos",
+    image:"image/momos.jpg",
+    category:"Fast-food",
+    price:"55 Rs.",
+    description:"My heart breaks when momo shop is closed.They look tasty but when You eat them they become"
+  },
+  {
+    id:5,
+    name:"Biryani",
+    image:"image/biryani.jpg",
+    category:"Lunch",
+    price:"100 Rs.",
+    description:" If someone says true love doesn't exist, make them taste biryani. "
+  },
+  {
+    id:6,
+    name:"Chai",
+    image:"image/chai.jpg",
+    category:"Drinks",
+    price:"10 Rs.",
+    description:" You must be completely awake in the present to enjoy the tea. "
+  },  
+  {
+    id:7,
+    name:"Coffee",
+    image:"image/coffee.jpg",
+    category:"Drinks",
+    price:"15 Rs.",
+    description:"It's amazing how the world begins to change through the eyes of a cup of coffee."
+  },
+  {
+    id:8,
+    name:"Cold Drinks",
+    image:"image/coke.jpg",
+    category:"Drinks",
+    price:"25 Rs.",
+    description:"A Coke is a Coke and no amount of money can get you a better Coke than the one the bum on the corner is drinking." 
+  },
+  {
+    id:9,
+    name:"Roti Rajma",
+    image:"image/roti_rajma.jpg",
+    category:"Dinner",
+    price:"99 Rs.",
+    description:"There are people in the world so hungry, that God cannot appear to them except in the form of bread."
+  },
+  {
+    id:10,
+    name:"Lassi",
+    image:"image/lassi.jpg",
+    category:"Drinks",
+    price:"30 Rs.",
+    description:"If someone makes curd of your brain then churn them to buttermilk and chill."
+  },
+  {
+    id:11,
+    name:"Veg Biryani",
+    image:"image/vegBiryani.jpg",
+    category:"Dinner",
+    price:"130 Rs.",
+    description:"If I accidentally become a chef, I will stop the process of adding elaichi's to biryani."
+  }
+];
+export default menuAPI;
